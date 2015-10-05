2934370afebc0b0adc75e1a98878d8e8f9bc553f
ICC-Margot <mar94.mlm@gmail.com>
2015-10-04 22:55:47 -0500

    Entrega Final

--- Matriz/src/main.adb
+++ Matriz/src/main.adb
@@ -1,31 +1,32 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure Main is
-   MatrizA : array (1..500,1..500) of Integer;
-   MatrizB : array (1..500,1..500) of Integer;
-   MatrizC : array (1..500,1..500) of Integer;
+   fin:Integer:=400;
+   MatrizA : array (1..fin,1..fin) of Integer;
+   MatrizB : array (1..fin,1..fin) of Integer;
+   MatrizC : array (1..fin,1..fin) of Integer;
    m: Integer:=0;
 begin
-   for i in 1..500 loop
-      for j in 1..500 loop
+   for i in 1..fin loop
+      for j in 1..fin loop
          MatrizA (i,j):= 2;
          MatrizB (i,j):= 2;
       end loop;
    end loop;
 
 
-   for k in 1..500 loop
-      for i in 1..500 loop
+   for k in 1..fin loop
+      for i in 1..fin loop
          m:=0;
-         for j in 1..500 loop
+         for j in 1..fin loop
            m:= MatrizA(k,j)+MatrizB(j,i)+m;
          end loop;
          MatrizC(k,i):=m;
       end loop;
    end loop;
 
-   for i in 1..500 loop
-      for j in 1..500 loop
+   for i in 1..fin loop
+      for j in 1..fin loop
          Put(MatrizC(i,j));
       end loop;
       Put_Line("");
--- Matriz2/src/main.adb
+++ Matriz2/src/main.adb
@@ -1,15 +1,14 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure Main is
-   MatrizA : array (1..5,1..5) of Integer;
-   MatrizB : array (1..5,1..5) of Integer;
-   MatrizC : array (1..5,1..5) of Integer;
+   fin:Integer:=400;
+   MatrizA : array (1..fin,1..fin) of Integer;
+   MatrizB : array (1..fin,1..fin) of Integer;
+   MatrizC : array (1..fin,1..fin) of Integer;
    m,i,j,k: Integer:=0;
 
-
-
    task type Multiplica is
-      entry iniciar (inicio,fin:Integer);
+      entry iniciar (inicio:Integer);
    end Multiplica;
 
    task MatrizF is
@@ -18,47 +17,56 @@ procedure Main is
 
    task LlenaMatriz;
 
-   task body LlenaMatriz is
-    begin
-     for i in 1..4 loop
-      for j in 1..4 loop
-         MatrizA (i,j):= 2;
-         MatrizB (i,j):= 2;
-         end loop;
-      end loop;
-   end LlenaMatriz;
 
    task body Multiplica is
       sumaR:Integer:=0;
    begin
-      accept iniciar (inicio: Integer; fin: Integer) do
-         for k in inicio..fin loop
+      accept iniciar (inicio: Integer) do
+         k:=inicio;
+         --for k in inicio..fin loop
           for i in 1..fin loop
             m:=0;
             for j in 1..fin loop
                 m:= MatrizA(k,j)+MatrizB(j,i)+m;
                end loop;
                MatrizF.posicion(k,i,m);
-          end loop;
          end loop;
+         --end loop;
       end iniciar;
    end Multiplica;
 
-   SubMatrices : array (0..4) of Multiplica;
+   SubMatrices : array (1..fin) of Multiplica;
+
+    task body LlenaMatriz is
+    begin
+     for i in 1..fin loop
+      for j in 1..fin loop
+         MatrizA (i,j):= 2;
+         MatrizB (i,j):= 2;
+         end loop;
+      end loop;
+
+      for i in 1..fin loop
+         SubMatrices(i).iniciar(i);
+      end loop;
+   end LlenaMatriz;
+
 
    task body MatrizF is
       resultado : Integer:=0;
    begin
-      for i in 0..4 loop
-       accept posicion (e,s, val:Integer) do
-         MatrizC(e,s):=val;
-       end posicion;
+      for i in 1..fin loop
+         for j in 1..fin loop
+          accept posicion (e,s, val:Integer) do
+           MatrizC(e,s):=val;
+          end posicion;
+         end loop;
       end loop;
 
-      Put_Line("La multiplicación es");
-      for i in 1..4 loop
-       for j in 1..4 loop
-         Put(MatrizC(i,j));
+      Put_Line("La multiplicación es: ");
+      for i in 1..fin loop
+       for j in 1..fin loop
+          Put(MatrizC(i,j));
        end loop;
        Put_Line("");
       end loop;
@@ -68,8 +76,5 @@ procedure Main is
 
 begin
    Put_Line("Multiplicaciòn de Matrices");
-   for i in 0..4 loop
-         SubMatrices(i).iniciar(i,4);
-      end loop;
    null;
 end Main;
